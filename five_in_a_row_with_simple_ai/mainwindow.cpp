41b40678d8b1525b2b084457c89af423be16fbd1
kingwl <805037171@163.com>
2014-10-21 16:53:55 +0800

    finish about game

--- five_in_a_row_with_simple_ai/mainwindow.cpp
+++ five_in_a_row_with_simple_ai/mainwindow.cpp
@@ -1,14 +1,10 @@
 #include "mainwindow.h"
-
+#define IS_AI
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
     resize(640, 480);
-    memset(map, 0, sizeof(map));
-    playerNow = PLAYER_H;
-    player_h_color = COLOR_BLACK;
-    player_c_color = COLOR_WHITE;
-
+    init();
 }
 MainWindow::~MainWindow()
 {
@@ -52,9 +48,24 @@ QPoint MainWindow::point2pos(QPoint point) const
 }
 void MainWindow::mousePressEvent(QMouseEvent *event)
 {
-    QPoint p = pos2point(event -> pos());
-    addPoint(p);
-    update();
+#ifdef IS_AI
+    if(stateNow == STATE_PLAY || stateNow == STATE_WAIT)
+#else
+    if(stateNow == STATE_PLAY)
+#endif
+    {
+        QPoint p = pos2point(event -> pos());
+        addPoint(p);
+        update();
+        int i = checkAll();
+        if(i != PLAYER_NONE){
+            stateNow = STATE_HOLD;
+            QMessageBox::information(0,"","end");
+        }
+    }else if(stateNow == STATE_HOLD){
+        init();
+        update();
+    }
 }
 QPoint MainWindow::point2pos(int x, int y) const
 {
@@ -70,6 +81,7 @@ void MainWindow::addPoint(QPoint pos)
     {
         map[pos.x()][pos.y()] = getPlayerColor(playerNow);
         changePlayer();
+        changeState();
     }
 
 }
@@ -91,9 +103,87 @@ void MainWindow::changePlayer()
         playerNow = PLAYER_C;
     }
 }
-bool MainWindow::check(QPoint p) const
+bool MainWindow::checkPoint(int x, int y) const
 {
+    int color = map[x][y];
+    if(color == COLOR_NONE) return false;
+    int num = 0;
+    for(int i = 1; i < 5; ++i)
+    {
+        if(is_in(x,y) && map[x + i][y] == color){
+            ++num;
+        }else{
+            break;
+        }
+    }
+    for(int i = 1; i < 5; ++i)
+    {
+        if(is_in(x,y) && map[x - i][y] == color){
+            ++num;
+        }else{
+            break;
+        }
+    }
+    if(num >= 4) return true;
+    num = 0;
+    for(int i = 1; i < 5; ++i)
+    {
+        if(is_in(x,y) && map[x][y + i] == color){
+            ++num;
+        }else{
+            break;
+        }
+    }
+    for(int i = 1; i < 5; ++i)
+    {
+        if(is_in(x,y) && map[x][y - i] == color){
+            ++num;
+        }else{
+            break;
+        }
+    }
+    if(num >= 4) return true;
+    num = 0;
+    for(int i = 1; i < 5; ++i)
+    {
+        if(is_in(x,y) && map[x + i][y + i] == color){
+            ++num;
+        }else{
+            break;
+        }
+    }
+    for(int i = 1; i < 5; ++i)
+    {
+        if(is_in(x,y) && map[x - i][y - i] == color){
+            ++num;
+        }else{
+            break;
+        }
+    }
+    if(num >= 4) return true;
+    num = 0;
+    for(int i = 1; i < 5; ++i)
+    {
+        if(is_in(x,y) && map[x + i][y - i] == color){
+            ++num;
+        }else{
+            break;
+        }
+    }
+    for(int i = 1; i < 5; ++i)
+    {
+        if(is_in(x,y) && map[x - i][y + i] == color){
+            ++num;
+        }else{
+            break;
+        }
+    }
+    if(num >= 4) return true;
     return false;
+}
+bool MainWindow::checkPoint(QPoint p) const
+{
+    return checkPoint(p.x(), p.y());
 }
 bool MainWindow::is_in(QPoint p) const
 {
@@ -103,4 +193,42 @@ bool MainWindow::is_in(int x, int y) const
 {
     if(x < 0 || y < 0 || x >= MAX_SIZE || y >= MAX_SIZE) return false;
     return true;
+}
+int MainWindow::checkAll() const
+{
+    for(int i = 0; i < MAX_SIZE; ++i)
+    {
+        for(int j = 0; j < MAX_SIZE; ++j)
+        {
+            if(checkPoint(i, j) == true){
+                return getPlayerByColor(map[i][j]);
+            }
+        }
+    }
+    return COLOR_NONE;
+}
+int MainWindow::getPlayerByColor(int color) const
+{
+    if(color == player_c_color){
+        return PLAYER_C;
+    }else if(color == player_h_color){
+        return PLAYER_H;
+    }
+    return PLAYER_NONE;
+}
+void MainWindow::init()
+{
+    memset(map, 0, sizeof(map));
+    playerNow = PLAYER_H;
+    player_h_color = COLOR_BLACK;
+    player_c_color = COLOR_WHITE;
+    stateNow = STATE_PLAY;
+}
+void MainWindow::changeState()
+{
+    if(stateNow == STATE_PLAY){
+        stateNow = STATE_WAIT;
+    }else if(stateNow == STATE_WAIT){
+        stateNow = STATE_PLAY;
+    }
 }
--- five_in_a_row_with_simple_ai/mainwindow.h
+++ five_in_a_row_with_simple_ai/mainwindow.h
@@ -13,11 +13,17 @@ class MainWindow : public QMainWindow
     static const int COLOR_NONE = 0;
     static const int COLOR_WHITE = 1;
     static const int COLOR_BLACK = 2;
-    int PLAYER_H;
-    int PLAYER_C;
+    static const int PLAYER_NONE = 0;
+    static const int PLAYER_H = 1;
+    static const int PLAYER_C = 2;
+    static const int STATE_HOLD = 0;
+    static const int STATE_WAIT = 1;
+    static const int STATE_PLAY = 2;
     int player_c_color;
     int player_h_color;
     int playerNow;
+    int stateNow;
+
 public:
     MainWindow(QWidget *parent = 0);
     ~MainWindow();
@@ -30,9 +36,14 @@ public:
     void addPoint(QPoint pos);
     int getPlayerColor(int player) const;
     void changePlayer();
-    bool check(QPoint p) const;
+    bool checkPoint(int x, int y) const;
+    bool checkPoint(QPoint p) const;
+    int checkAll() const;
     bool is_in(QPoint p) const;
     bool is_in(int x, int y) const;
+    int getPlayerByColor(int color) const;
+    void init();
+    void changeState();
 private:
     int map[MAX_SIZE][MAX_SIZE];
 
